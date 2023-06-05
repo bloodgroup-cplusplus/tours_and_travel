d47b9f4b59974d9c590dfdbcf47b659676e6f4b9
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-06 03:11:22 +0530

    added header and footer components

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPRwlvzT1wP0iL9FnWcZdFPB5ksZHmeNc",
+  authDomain: "trabel-f1850.firebaseapp.com",
+  projectId: "trabel-f1850",
+  storageBucket: "trabel-f1850.appspot.com",
+  messagingSenderId: "476243956965",
+  appId: "1:476243956965:web:fb3314ab657d793e10f00c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
