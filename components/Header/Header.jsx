4d2162f35c81753ca6f7c9cd9fdea5656f814740
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-06 03:56:43 +0530

    added bunch of stuff

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,8 +1,35 @@
+'use client'
 import React from "react";
+import { Container,Row,Button } from "reactstrap";
+import Link from "next/link";
+import next_logo from "../../public/next.svg"
 
 const Header=()=>{
     return (
-        <div>Header</div>
+        <header className="header">
+            <Container>
+                <Row>
+                    <div className="nav_wrapper d-flex align-items-center justify-content-between">
+                        <div className="logo">
+                            <img src={next_logo} alt="Tours and Travel Logo"/>
+                        </div>
+                        <div className="navigation">
+                            <ul className="menu d-flex align-items-center gap-5">
+                                <Link href="/">Home</Link>
+                                <Link href="/login">Login</Link>
+                                <Link href="/register">Register</Link>
+                                <Link href="/tours">Tours</Link>                                
+                            </ul>
+                        </div>
+
+                        <div className="nav__right d-flex align-items-center gap-4">
+
+                        </div>
+
+                    </div>
+                </Row>
+                </Container>
+        </header>
     )
 }
 
